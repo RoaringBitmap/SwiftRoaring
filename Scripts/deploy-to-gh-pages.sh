1a4dacabf92ae3cae9070c0b002f5188b99c04ee
piotte13 <jeremie.piotte@gmail.com>
2018-09-19 11:30:47 -0400

    debugging travis

--- Scripts/deploy-to-gh-pages.sh
+++ Scripts/deploy-to-gh-pages.sh
@@ -7,7 +7,7 @@ mkdir public
 
 # config
 git config --global user.email "travis@swiftroaring.com"
-git config --global user.name "Travis CI"
+git config --global user.name "Travis-CI"
 
 # build (CHANGE THIS)
 jazzy
@@ -17,4 +17,4 @@ cd docs
 git init
 git add .
 git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+git push --force "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages
