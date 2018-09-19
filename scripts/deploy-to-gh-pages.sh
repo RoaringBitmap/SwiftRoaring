4c229f9a5df4d20ffbeeecf7fbcc362733249144
piotte13 <jeremie.piotte@gmail.com>
2018-09-19 11:08:43 -0400

    github pages automation

---
+++ scripts/deploy-to-gh-pages.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
+set -o errexit
+
+rm -rf public
+mkdir public
+
+# config
+git config --global user.email "travis@swiftroaring.com"
+git config --global user.name "Travis CI"
+
+# build (CHANGE THIS)
+jazzy
+
+# deploy
+cd docs
+git init
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
