96968691c431defa975c1c364b3dcbf8bffa1509
adamnemecek <adamnemecek@gmail.com>
2022-05-18 08:01:21 -0700

    fixed foramtting

--- Tests/swiftRoaringTests/swiftRoaringTests.swift
+++ Tests/swiftRoaringTests/swiftRoaringTests.swift
@@ -1,9 +1,8 @@
 import XCTest
 @testable import SwiftRoaring
 
-
 extension swiftRoaringTests {
-    static var allTests : [(String, (swiftRoaringTests) -> () throws -> Void)] {
+    static var allTests: [(String, (swiftRoaringTests) -> () throws -> Void)] {
         return [
             ("testAdd", testAdd),
             ("testRemove", testRemove),
@@ -28,9 +27,9 @@ extension swiftRoaringTests {
             ("testPortableSerialize", testPortableSerialize),
             ("testStatistics", testStatistics),
             ("testJaccardIndex", testJaccardIndex),
-            ("testDescription", testDescription), 
-            ("testHashValue", testHashValue),   
-            ("testBase64", testBase64),    
+            ("testDescription", testDescription),
+            ("testHashValue", testHashValue),
+            ("testBase64", testBase64)
 
         ]
     }
@@ -73,51 +72,51 @@ class swiftRoaringTests: XCTestCase {
         for i in rbm {
             XCTAssertEqual(rbm.contains(i), true)
             count -= 1
-            if(count < 0) {break}
+            if count < 0 {break}
         }
         XCTAssertEqual(count, 0)
     }
 
-    func testInitRange(){
-        let rbmRange = RoaringBitmap(min: 0,max: 1000,step: 50)
+    func testInitRange() {
+        let rbmRange = RoaringBitmap(min: 0, max: 1000, step: 50)
         for k in stride(from: 0, to: 1000, by: 50 ) {
             XCTAssertEqual(rbmRange.contains(UInt32(k)), true)
         }
     }
 
-    func testInitCapacity(){
+    func testInitCapacity() {
         let rbmCapacity = RoaringBitmap(capacity: 8)
         XCTAssertEqual(rbmCapacity.sizeInBytes(), 5)
     }
 
-    func testInitArray(){
-        let array = [0,1,2,4,5,6]
-        let rbmArray = RoaringBitmap(values: array.map{ UInt32($0) })
+    func testInitArray() {
+        let array = [0, 1, 2, 4, 5, 6]
+        let rbmArray = RoaringBitmap(values: array.map { UInt32($0) })
         for i in array {
             XCTAssertEqual(rbmArray.contains(UInt32(i)), true)
         }
-        let l: RoaringBitmap = [0,1,2,4,5,6]
+        let l: RoaringBitmap = [0, 1, 2, 4, 5, 6]
         for i in array {
             XCTAssertEqual(l.contains(UInt32(i)), true)
         }
     }
 
-    func testFlip(){
-        rbm.addRangeClosed(min:0, max:500)
-        let flip = rbm.flip(rangeStart: 0, rangeEnd:501)
+    func testFlip() {
+        rbm.addRangeClosed(min: 0, max: 500)
+        let flip = rbm.flip(rangeStart: 0, rangeEnd: 501)
         XCTAssertTrue(flip.isEmpty)
-        rbm.flipInplace(rangeStart: 0, rangeEnd:501)
+        rbm.flipInplace(rangeStart: 0, rangeEnd: 501)
         XCTAssertTrue(rbm.isEmpty)
     }
 
-    func testEquals(){
+    func testEquals() {
         let cpy = rbm.copy()
         XCTAssertTrue(cpy.equals(rbm))
         XCTAssertTrue(cpy == rbm)
         XCTAssertEqual(cpy != rbm, false)
     }
 
-    func testSubset(){
+    func testSubset() {
         let cpy = rbm.copy()
         XCTAssertTrue(rbm.isSubset(of: cpy))
         cpy.add(800)
@@ -125,22 +124,22 @@ class swiftRoaringTests: XCTestCase {
         cpy.remove(800)
     }
 
-    func testOptimisations(){
-        rbm.addRangeClosed(min:0, max:500)
+    func testOptimisations() {
+        rbm.addRangeClosed(min: 0, max: 500)
         XCTAssertTrue(rbm.sizeInBytes() > 0)
         XCTAssertTrue(rbm.shrink() >= 0)
         XCTAssertTrue(rbm.runOptimize())
         XCTAssertTrue(rbm.removeRunCompression())
     }
 
-    func testPrinting(){
+    func testPrinting() {
         let rbmap = RoaringBitmap()
         rbmap.add(1)
         rbmap.describe()
         rbmap.print()
     }
 
-    func testToArray(){
+    func testToArray() {
         rbm.add(35)
         var array = rbm.toArray()
         for i in rbm {
@@ -152,10 +151,10 @@ class swiftRoaringTests: XCTestCase {
         XCTAssertTrue(rbm.count == 1)
     }
 
-    func testAddingRemoving(){
-        rbm.addRangeClosed(min:0, max:500)
+    func testAddingRemoving() {
+        rbm.addRangeClosed(min: 0, max: 500)
         let cpy = rbm.copy()
-        _ = cpy.containsRange(start:0, end:501)
+        _ = cpy.containsRange(start: 0, end: 501)
         XCTAssertEqual(cpy.max(), 500)
         XCTAssertEqual(cpy.min(), 0)
         XCTAssertEqual(cpy.rank(value: 499), 500)
@@ -168,19 +167,19 @@ class swiftRoaringTests: XCTestCase {
         rbmap.removeRangeClosed(min: 0, max: 10)
         XCTAssertTrue(rbmap.count == 0)
         XCTAssertTrue(rbmap.addCheck(0))
-        rbmap.addMany(values: [1,2,3])
+        rbmap.addMany(values: [1, 2, 3])
         XCTAssertTrue(rbmap.count == 4)
         XCTAssertTrue(rbmap.removeCheck(3))
         XCTAssertTrue(rbmap.count == 3)
     }
 
-    func testSelect(){
-        rbm.addRangeClosed(min:0, max:500)
-        XCTAssertTrue(rbm.select(rank:5, value: 800))
-        XCTAssertEqual(rbm.rank(value:800), 501)
+    func testSelect() {
+        rbm.addRangeClosed(min: 0, max: 500)
+        XCTAssertTrue(rbm.select(rank: 5, value: 800))
+        XCTAssertEqual(rbm.rank(value: 800), 501)
     }
 
-    func testAnd(){
+    func testAnd() {
         let (rbm1, rbm2, swiftSet1, swiftSet2) = makeSets()
 
         let andRbm = rbm1 & rbm2
@@ -193,10 +192,10 @@ class swiftRoaringTests: XCTestCase {
         let andCardinality = rbm1.intersectionCount(rbm2)
         XCTAssertEqual(Int(andCardinality), andSwift.count)
 
-        XCTAssertEqual(rbm1.intersect(rbm2), andSwift.count > 0)   
+        XCTAssertEqual(rbm1.intersect(rbm2), andSwift.count > 0)
     }
 
-    func testOr(){
+    func testOr() {
         let (rbm1, rbm2, swiftSet1, swiftSet2) = makeSets()
 
         let andRbm = rbm1 | rbm2
@@ -207,13 +206,13 @@ class swiftRoaringTests: XCTestCase {
         XCTAssertEqual(Int(orCardinality), andSwift.count)
 
         let (rbm3, rbm4, swiftSet3, swiftSet4) = makeSets()
-        var orMany = rbm1.unionMany([rbm2,rbm3,rbm4])
+        var orMany = rbm1.unionMany([rbm2, rbm3, rbm4])
         var swiftOrMany = swiftSet1.union(swiftSet2)
         swiftOrMany = swiftOrMany.union(swiftSet3)
         swiftOrMany = swiftOrMany.union(swiftSet4)
         XCTAssertEqual(swiftOrMany, Set(orMany.toArray()))
 
-        orMany = rbm1.unionManyHeap([rbm2,rbm3,rbm4])
+        orMany = rbm1.unionManyHeap([rbm2, rbm3, rbm4])
         XCTAssertEqual(swiftOrMany, Set(orMany.toArray()))
 
         let lazy = rbm3.lazyUnion(rbm4, bitsetconversion: false)
@@ -226,7 +225,7 @@ class swiftRoaringTests: XCTestCase {
         XCTAssertEqual(andSwift, Set(rbm1.toArray()))
     }
 
-    func testXor(){
+    func testXor() {
         let (rbm1, rbm2, swiftSet1, swiftSet2) = makeSets()
 
         let andRbm = rbm1 ^ rbm2
@@ -237,7 +236,7 @@ class swiftRoaringTests: XCTestCase {
         XCTAssertEqual(Int(xorCardinality), andSwift.count)
 
         let (rbm3, rbm4, swiftSet3, swiftSet4) = makeSets()
-        let orMany = rbm1.symmetricDifferenceMany([rbm2,rbm3,rbm4])
+        let orMany = rbm1.symmetricDifferenceMany([rbm2, rbm3, rbm4])
         var swiftOrMany = swiftSet1.symmetricDifference(swiftSet2)
         swiftOrMany = swiftOrMany.symmetricDifference(swiftSet3)
         swiftOrMany = swiftOrMany.symmetricDifference(swiftSet4)
@@ -253,7 +252,7 @@ class swiftRoaringTests: XCTestCase {
         XCTAssertEqual(andSwift, Set(rbm1.toArray()))
     }
 
-    func testAndNot(){
+    func testAndNot() {
         let (rbm1, rbm2, swiftSet1, swiftSet2) = makeSets()
 
         let andRbm = rbm1 - rbm2
@@ -267,8 +266,8 @@ class swiftRoaringTests: XCTestCase {
         XCTAssertEqual(Int(andNotCardinality), andSwift.count)
     }
 
-    func testSerialize(){
-        rbm.addRangeClosed(min:0, max:500)
+    func testSerialize() {
+        rbm.addRangeClosed(min: 0, max: 500)
         let size = rbm.sizeInBytes()
         var buffer = [Int8](repeating: 0, count: size)
         XCTAssertEqual(rbm.serialize(buffer: &buffer), size)
@@ -277,8 +276,8 @@ class swiftRoaringTests: XCTestCase {
 
     }
 
-    func testPortableSerialize(){
-        rbm.addRangeClosed(min:0, max:500)
+    func testPortableSerialize() {
+        rbm.addRangeClosed(min: 0, max: 500)
         let size = rbm.portableSizeInBytes()
         var buffer = [Int8](repeating: 0, count: size)
         XCTAssertEqual(rbm.portableSerialize(buffer: &buffer), size)
@@ -289,15 +288,15 @@ class swiftRoaringTests: XCTestCase {
         XCTAssertTrue(deserializedSafeRbm == rbm)
     }
 
-    func testBase64(){
+    func testBase64() {
         let bitmap = RoaringBitmap()
-        for i in 0..<50 { 
+        for i in 0..<50 {
           // from https://github.com/RoaringBitmap/SwiftRoaring/issues/1
           let random = Int.random(in: 0...1)
           if random == 0 {
             bitmap.add(UInt32(i))
           }
-        } 
+        }
         let size = rbm.portableSizeInBytes()
         var buffer = [Int8](repeating: 0, count: size)
         XCTAssertEqual(rbm.portableSerialize(buffer: &buffer), size)
@@ -313,32 +312,30 @@ class swiftRoaringTests: XCTestCase {
         XCTAssertTrue(deserializedSafeRbm == rbm)
     }
 
-
-    func testStatistics(){
-        rbm.addRangeClosed(min:0, max:500)
+    func testStatistics() {
+        rbm.addRangeClosed(min: 0, max: 500)
         let stats = rbm.statistics()
         XCTAssertTrue(stats.max_value == 500)
         XCTAssertTrue(stats.min_value == 0)
         XCTAssertTrue(stats.cardinality == 501)
     }
 
-    func testJaccardIndex(){
-        rbm.addMany(values: [1,2,3])
-        let rbm2 = RoaringBitmap(values: [3,4,5])
+    func testJaccardIndex() {
+        rbm.addMany(values: [1, 2, 3])
+        let rbm2 = RoaringBitmap(values: [3, 4, 5])
         XCTAssertEqual(rbm.jaccardIndex(rbm2), 0.2)
     }
 
-    func testDescription(){
-        rbm.addRangeClosed(min:0, max:500)
+    func testDescription() {
+        rbm.addRangeClosed(min: 0, max: 500)
         print(rbm.description)
     }
 
-    func testHashValue(){
+    func testHashValue() {
         print(rbm.hashValue)
     }
 
-
-    func makeSets() -> (RoaringBitmap, RoaringBitmap, Set<UInt32>, Set<UInt32>){
+    func makeSets() -> (RoaringBitmap, RoaringBitmap, Set<UInt32>, Set<UInt32>) {
         let randList1 = makeList(100)
         let randList2 = makeList(100)
         let rbm1 = RoaringBitmap(values: randList1)
@@ -351,9 +348,9 @@ class swiftRoaringTests: XCTestCase {
 
     func makeList(_ n: Int) -> [UInt32] {
          #if os(Linux)
-            return (0..<n).map{ _ in UInt32(Int.random(in: 0 ..< 1000)) }
+            return (0..<n).map { _ in UInt32(Int.random(in: 0 ..< 1000)) }
         #else
-            return (0..<n).map{ _ in UInt32(arc4random_uniform(1000)+1) }
+            return (0..<n).map { _ in UInt32(arc4random_uniform(1000)+1) }
         #endif
     }
 }
